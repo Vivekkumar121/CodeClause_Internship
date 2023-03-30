8f575daffc3b05f2e192500ad9d587fba1ce4f2a
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-03-30 20:31:55 +0530

    Add files via upload

---
+++ Project_2/BookServiceInterface.java
@@ -0,0 +1,11 @@
+public interface BookServiceInterface {
+    void addBook();                           //the menu functions
+
+    void showAllBooks();
+
+    void showAllAvailableBooks();
+
+    void borrowBook();
+
+    void returnBook();
+}
